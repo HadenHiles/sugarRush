9ebff7792dfd4e50860d136d304a3631534c1f76
Haden <HadenHiles@gmail.com>
2014-11-07 21:56:48 -0500

    -replaced old code with new object oriented code -added new assets/images -seeking out a js version of the createjs-lib.d.ts file so that the game will work again

--- Scripts/game.js
+++ Scripts/game.js
@@ -1,290 +1,78 @@
 /**
  * Created with IntelliJ IDEA.
  * User: HandsHiles
- * Date: 14-11-05
- * Time: 9:50 PM
+ * Date: 14-11-07
+ * Time: 6:55 PM
  * To change this template use File | Settings | File Templates.
  */
 var stage;
-var queue;
+var game;
 
-// Game Objects
+var background;
 var plane;
 var island;
-var Background;
+var clouds = [];
 var scoreboard;
 
-// Cloud Array
-var clouds = [];
+var collision;
 
-// Game Constants
-var CLOUD_NUM = 3;
-var GAME_FONT = "30px Consolas";
-var FONT_COLOUR = "#FFFF00";
-var PLAYER_LIVES = 3;
+var tryAgain;
+var playButton;
 
+var currentState;
+var currentStateFunction;
+
+// Preload function - Loads Assets and initializes game;
 function preload() {
-    queue = new createjs.LoadQueue();
-    queue.installPlugin(createjs.Sound);
-    queue.addEventListener("complete", init);
-    queue.loadManifest([
-        { id: "plane", src: "images/plane.png" },
-        { id: "island", src: "images/island.png" },
-        { id: "cloud", src: "images/cloud.png" },
-        { id: "background", src: "images/bg.png" },
-        { id: "yay", src: "sounds/yay.ogg" },
-        { id: "thunder", src: "sounds/thunder.ogg" },
-        { id: "engine", src: "sounds/engine.ogg" }
-    ]);
+    managers.Assets.init();
+    managers.Assets.loader.addEventListener("complete", init);
 }
 
+// init called after Assets have been loaded.
 function init() {
     stage = new createjs.Stage(document.getElementById("canvas"));
-    stage.enableMouseOver(20);
+    stage.enableMouseOver(30);
     createjs.Ticker.setFPS(60);
     createjs.Ticker.addEventListener("tick", gameLoop);
-    gameStart();
+    optimizeForMobile();
+
+    currentState = constants.MENU_STATE;
+    changeState(currentState);
+}
+
+// Add touch support for mobile devices
+function optimizeForMobile() {
+    if (createjs.Touch.isSupported()) {
+        createjs.Touch.enable(stage);
+    }
 }
 
 // Game Loop
 function gameLoop(event) {
-    background.update();
-    island.update();
-    plane.update();
-
-    for (var count = 0; count < CLOUD_NUM; count++) {
-        clouds[count].update();
-    }
-
-    collisionCheck();
-
-    scoreboard.update();
-
+    currentStateFunction();
     stage.update();
 }
 
-// Plane Class
-var Plane = (function () {
-    function Plane() {
-        this.image = new createjs.Bitmap(queue.getResult("plane"));
-        this.width = this.image.getBounds().width;
-        this.height = this.image.getBounds().height;
-        this.image.regX = this.width * 0.5;
-        this.image.regY = this.height * 0.5;
-        this.image.y = 430;
+function changeState(state) {
+    switch (state) {
+        case constants.MENU_STATE:
+            // instantiate menu screen
+            currentStateFunction = states.menuState;
+            states.menu();
+            break;
 
-        stage.addChild(this.image);
+        case constants.PLAY_STATE:
+            // instantiate play screen
+            currentStateFunction = states.playState;
+            states.play();
+            break;
 
-        // Play engine sound forever
-        createjs.Sound.play("engine", 0, 0, 0, -1, 1, 0);
-    }
-    Plane.prototype.update = function () {
-        this.image.x = stage.mouseX;
-        this.image.y = stage.mouseY;
-    };
-    return Plane;
-})();
+        case constants.GAME_OVER_STATE:
+            currentStateFunction = states.gameOverState;
 
-// Island Class
-var Island = (function () {
-    function Island() {
-        this.image = new createjs.Bitmap(queue.getResult("island"));
-        this.width = this.image.getBounds().width;
-        this.height = this.image.getBounds().height;
-        this.image.regX = this.width * 0.5;
-        this.image.regY = this.height * 0.5;
-        this.dy = 5;
-        stage.addChild(this.image);
-        this.reset();
-    }
-    Island.prototype.reset = function () {
-        this.image.y = -this.height;
-        this.image.x = Math.floor(Math.random() * stage.canvas.width);
-    };
-
-    Island.prototype.update = function () {
-        this.image.y += this.dy;
-        if (this.image.y >= (this.height + stage.canvas.height)) {
-            this.reset();
-        }
-    };
-    return Island;
-})();
-
-// Cloud Class
-var Cloud = (function () {
-    function Cloud() {
-        this.image = new createjs.Bitmap(queue.getResult("cloud"));
-        this.width = this.image.getBounds().width;
-        this.height = this.image.getBounds().height;
-        this.image.regX = this.width * 0.5;
-        this.image.regY = this.height * 0.5;
-        stage.addChild(this.image);
-        this.reset();
-    }
-    Cloud.prototype.reset = function () {
-        this.image.y = -this.height;
-        this.image.x = Math.floor(Math.random() * stage.canvas.width);
-        this.dy = Math.floor(Math.random() * 5 + 5);
-        this.dx = Math.floor(Math.random() * 4 - 2);
-    };
-
-    Cloud.prototype.update = function () {
-        this.image.y += this.dy;
-        this.image.x += this.dx;
-        if (this.image.y >= (this.height + stage.canvas.height)) {
-            this.reset();
-        }
-    };
-    return Cloud;
-})();
-
-// Background Class
-var Background = (function () {
-    function Background() {
-        this.image = new createjs.Bitmap(queue.getResult("background"));
-        this.width = this.image.getBounds().width;
-        this.height = this.image.getBounds().height;
-        this.dy = 5;
-        stage.addChild(this.image);
-        this.reset();
-    }
-    Background.prototype.reset = function () {
-        this.image.y = -960;
-    };
-
-    Background.prototype.update = function () {
-        this.image.y += this.dy;
-        if (this.image.y >= 0) {
-            this.reset();
-        }
-    };
-    return Background;
-})();
-
-// The Distance Utility Function
-function distance(p1, p2) {
-    var firstPoint;
-    var secondPoint;
-    var theXs;
-    var theYs;
-    var result;
-
-    firstPoint = new createjs.Point();
-    secondPoint = new createjs.Point();
-
-    firstPoint.x = p1.x;
-    firstPoint.y = p1.y;
-
-    secondPoint.x = p2.x;
-    secondPoint.y = p2.y;
-
-    theXs = secondPoint.x - firstPoint.x;
-    theYs = secondPoint.y - firstPoint.y;
-
-    theXs = theXs * theXs;
-    theYs = theYs * theYs;
-
-    result = Math.sqrt(theXs + theYs);
-
-    return result;
-}
-
-// Check Collision between Plane and Island
-function planeAndIsland() {
-    var point1 = new createjs.Point();
-    var point2 = new createjs.Point();
-
-    point1.x = plane.image.x;
-    point1.y = plane.image.y;
-    point2.x = island.image.x;
-    point2.y = island.image.y;
-    if (distance(point1, point2) < ((plane.height * 0.5) + (island.height * 0.5))) {
-        createjs.Sound.play("yay");
-        scoreboard.score += 100;
-        island.reset();
+            // instantiate game over screen
+            states.gameOver();
+            break;
     }
 }
-
-// Check Collision between Plane and Cloud
-function planeAndCloud(theCloud) {
-    var point1 = new createjs.Point();
-    var point2 = new createjs.Point();
-    var cloud = new Cloud();
-
-    cloud = theCloud;
-
-    point1.x = plane.image.x;
-    point1.y = plane.image.y;
-    point2.x = cloud.image.x;
-    point2.y = cloud.image.y;
-    if (distance(point1, point2) < ((plane.height * 0.5) + (cloud.height * 0.5))) {
-        createjs.Sound.play("thunder");
-        scoreboard.lives -= 1;
-        cloud.reset();
-    }
-}
-
-// Collision Check Utility Function
-function collisionCheck() {
-    planeAndIsland();
-
-    for (var count = 0; count < CLOUD_NUM; count++) {
-        planeAndCloud(clouds[count]);
-    }
-}
-
-var Scoreboard = (function () {
-    function Scoreboard() {
-        this.labelString = "";
-        this.lives = PLAYER_LIVES;
-        this.score = 0;
-        this.label = new createjs.Text(this.labelString, GAME_FONT, FONT_COLOUR);
-        this.update();
-        this.width = this.label.getBounds().width;
-        this.height = this.label.getBounds().height;
-
-        stage.addChild(this.label);
-    }
-    Scoreboard.prototype.update = function () {
-        this.labelString = "Lives: " + this.lives.toString() + " Score: " + this.score.toString();
-        this.label.text = this.labelString;
-    };
-    return Scoreboard;
-})();
-
-// Main Game Function
-function gameStart() {
-    var point1 = new createjs.Point();
-    var point2 = new createjs.Point();
-
-    background = new Background();
-    island = new Island();
-    plane = new Plane();
-
-    for (var count = 0; count < CLOUD_NUM; count++) {
-        clouds[count] = new Cloud();
-    }
-
-    scoreboard = new Scoreboard();
-}
-var Greeter = (function () {
-    function Greeter(message) {
-        this.greeting = message;
-    }
-    Greeter.prototype.greet = function () {
-        return "Hello, " + this.greeting;
-    };
-    return Greeter;
-})();
-
-var greeter = new Greeter("world");
-
-var button = document.createElement('button');
-button.textContent = "Say Hello";
-button.onclick = function () {
-    alert(greeter.greet());
-};
-
-document.body.appendChild(button);
 
